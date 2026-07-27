a1a039fc0a8bc0668a090dc6bac967b6fb310fd8
Legion Cunningham <legioncunningham@gmail.com>
2026-07-27 21:39:29 +0200

    Configuration file for Flask application

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-# Flask app factory, config, run
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
