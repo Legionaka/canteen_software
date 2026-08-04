64a8d4b5d38173146a20316c6f28f10f883ea0e9
Legion Cunningham <legioncunningham@gmail.com>
2026-08-04 19:52:14 +0200

    Small changes made to the student routes to improve functionality and fix minor bugs.

--- routes/student.py
+++ routes/student.py
@@ -1,3 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from utils import login_required, role_required
+from models import db, MenuItem
 
 student_bp = Blueprint('student', __name__)
